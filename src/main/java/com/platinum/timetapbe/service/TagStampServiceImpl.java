d8ca224e56448972ce9b62e1731539834246763a
Giuseppe Spina <giuseppespina@192.168.88.13>
2024-05-27 20:44:12 +0200

    ultimation-tagstamp

--- src/main/java/com/platinum/timetapbe/service/TagStampServiceImpl.java
+++ src/main/java/com/platinum/timetapbe/service/TagStampServiceImpl.java
@@ -1,10 +1,13 @@
 package com.platinum.timetapbe.service;
 
 import com.platinum.timetapbe.documents.TagStamp;
+import com.platinum.timetapbe.documents.User;
 import com.platinum.timetapbe.dto.TagStampRequest;
 import com.platinum.timetapbe.repository.TagStampRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class TagStampServiceImpl implements ITagStampService {
@@ -13,10 +16,12 @@ public class TagStampServiceImpl implements ITagStampService {
     private TagStampRepository tagStampRepository;
 
     @Override
-    public TagStamp saveNewTagStamp(TagStampRequest request) {
+    public TagStamp saveNewTagStamp(User user, TagStampRequest request) {
         TagStamp tagStampToSave = new TagStamp();
         tagStampToSave.setPositionLabel(request.getPositionLabel());
         tagStampToSave.setCoordinates(request.getCoordinates());
+        tagStampToSave.setTimeCode(new Date());
+        tagStampToSave.setUser(user);
         return this.tagStampRepository.save(tagStampToSave);
     }
 }
